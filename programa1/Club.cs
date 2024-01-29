0e8145871aa04d65766ef0396a9cc48a610e95b3
noeury <noeuryfeliz@hotmail.com>
2024-01-29 11:39:12 -0400

    Add project files.

---
+++ programa1/Club.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa1
+{
+    internal class Club
+    {
+        Socio socio1 = new Socio("Jose", 2);
+        Socio socio2 = new Socio("Miguel", 3);
+        Socio socio3 = new Socio("Marcos", 1);
+
+
+    }
+}
---
+++ programa1/Socio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa1
+{
+    internal class Socio
+    {
+        private string nombre;
+        private int antiguedad;
+
+        public Socio(string nombre, int antiguedad)
+        {
+            this.nombre = nombre;
+            this.antiguedad = antiguedad;
+        }
+    }
+}
